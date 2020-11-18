c6c5ca0d85aeb0e34fe53937abd919f76ee8ffb4
Francisco do Carmo Amorim <74668512+Duca2020@users.noreply.github.com>
2020-11-18 17:32:02 -0300

    Add files via upload

---
+++ Ex04b_18112020.py
@@ -0,0 +1,5 @@
+#Resolução do segundo exercício...
+
+fah = float(input("Digite o valor da temperatura (°F): \n"))
+cel = (fah-32)*5/9
+print(f"O valor correspondente de {fah}°F em Celsius é: {cel}°C.")
