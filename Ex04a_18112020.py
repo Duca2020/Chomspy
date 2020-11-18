deeea6181d69c39307875ac3537688f39c4e12a0
Francisco do Carmo Amorim <74668512+Duca2020@users.noreply.github.com>
2020-11-18 17:38:31 -0300

    Delete Ex04a_18112020.py

--- Ex04a_18112020.py
+++
@@ -1,7 +0,0 @@
-#Resolução do primeiro exercício...
-
-height = float(input("Digite a altura do retângulo: \n"))
-width = float(input("Digite a largura do retângulo: \n"))
-print(f"A área do retângulo é: {height*width} u.m.2")
-
-#u.m. é a representação génerica para unidade de medida, nesse caso , por se tratar de duas dimensões temos u.m.^2
